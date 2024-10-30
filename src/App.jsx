012210098076240d56047cbd31dfe842432a4ee1
Gabriela1030 <gabriela518119@gmail.com>
2024-10-29 23:49:48 -0500

    merge featurehome and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 import Home from './components/home/Home';
 import Dogs from './components/Dogs';
 import Cats from './components/Cats';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/home/styles.css';
-import Header from './components/home/Header'; 
 
 const App = () => {
   const [currentView, setCurrentView] = useState('home');
@@ -22,7 +22,51 @@ const App = () => {
 
   return (
     <div className="min-vh-100 d-flex flex-column">
-      <Header /> 
+      <Navbar expand="lg" className="bg-white py-3">
+        <div className="container-fluid px-4">
+          <Navbar.Brand href="#" className="d-flex align-items-center">
+            <div className="brand-text">
+              
+            </div>
+            <img 
+              src="./assets/logo.png" 
+              alt="Logo"
+              className="ms-2"
+              style={{ width: '30px', height: '30px' }}
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link 
+                onClick={() => setCurrentView('home')}
+                className="nav-link-custom"
+              >
+                Inicio
+              </Nav.Link>
+              <Nav.Link 
+                onClick={() => setCurrentView('dogs')}
+                className="nav-link-custom"
+              >
+                Perros
+              </Nav.Link>
+              <Nav.Link 
+                onClick={() => setCurrentView('cats')}
+                className="nav-link-custom"
+              >
+                Gatos
+              </Nav.Link>
+              <Nav.Link 
+                href="#about"
+                className="nav-link-custom"
+              >
+                Sobre nosotros
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+
       <main className="flex-grow-1">
         {renderView()}
       </main>
